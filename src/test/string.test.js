0728736a17c91d54a358f240c7169c210d816b37
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-11-20 18:35:14 +0600

    added test for a reverse string

--- src/test/string.test.js
+++ src/test/string.test.js
@@ -3,3 +3,7 @@ const { capitalize } = require('../lib/string');
 test('returns string with the first character capitalized', () => {
   expect(capitalize('microverse')).toBe('Microverse');
 });
+
+test('returns string as reversed', () => {
+  expect(reverseString('cricket')).toBe('tekcirc');
+});
