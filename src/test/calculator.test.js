e23598c5ab9f982a1e5e2f3a89847ad88387598b
Shshamim63 <shossain142056@bscse.uiu.ac.bd>
2019-11-20 19:13:11 +0600

    added test for subtract

--- src/test/calculator.test.js
+++ src/test/calculator.test.js
@@ -3,3 +3,7 @@ const calculator = require('../lib/calculator');
 test('returns sum of two number', () => {
   expect(calculator.add(2, 5)).toBe(7);
 });
+
+test('returns subtract of two number', () => {
+  expect(calculator.subtract(7, 5)).toBe(2);
+});
